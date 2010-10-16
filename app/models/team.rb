4e818a679f3ae795248223a976a52d89369c54cf
Softa <contato@softa.com.br>
2010-10-16 09:24:42 -0400

    another commit without a significant comment. while in rumble do as rumblers.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,13 +1,11 @@
 class Team < ActiveRecord::Base
-<<<<<<< HEAD
+
   label :name, :team_home
-  has_many :users
+  has_many :team_memberships
+  has_many :users, :through => :team_memberships
+
   def url
     "http://railsrumble.com/teams/#{team_home}"
   end
+
 end
-=======
-  has_many :team_memberships
-  has_many :users, :through => :team_memberships
-end
->>>>>>> 6da3aa7cb62edcdfd2d80103173f96a73015fee1

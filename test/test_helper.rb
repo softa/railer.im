0b20852c91586e073b285b58ca72efb82e95190a
Diogo Biazus <diogob@gmail.com>
2010-10-16 02:51:41 -0300

    refactored tests. stop using max from database to generate unique ids

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,24 +10,25 @@ class ActiveSupport::TestCase
   fixtures = false
 
   # Gets the next id for model
-  def get_id(model)
-    (model.maximum(:id) || 0) + 1
+  def get_id
+    @unique_id = (@unique_id || 0) + 1
   end
 
   # Creates a test user with default values
   def create_user(params = {})
-    id = get_id(User)
-    defaults = {:name => "test user #{id}", :email => "test_user#{id}@railer.im", :company_name => "co test", :public_repo_count => 1, :blog => 'http://blog.user.com', :github_id => 'repo-1212', :public_gist_count => 1, :login => "test_user_#{id}",
+    defaults = {:name => "test user #{get_id}", :email => "test_user#{get_id}@railer.im", :company_name => "co test", :public_repo_count => 1, :blog => 'http://blog.user.com', :github_id => 'repo-1212', :public_gist_count => 1, :login => "test_user_#{get_id}",
     :password => 'testing_password_123', :password_confirmation => 'testing_password_123' }
     defaults.merge(params)
     User.create!(defaults)
   end
 
   def create_repository(params = {})
-    id = get_id(Repository)
     owner = create_user
-    defaults = {:name => "repo #{id}", :user => owner}
+    defaults = {:name => "repo #{get_id}", :user => owner}
     defaults.merge(params)
     Repository.create!(defaults)
+  end
+
+  def create_twitter_profile(params ={})
   end
 end

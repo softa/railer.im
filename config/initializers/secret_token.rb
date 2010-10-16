6afb44dc4412babf31cf149d39cfa20cc3645484
Softa <contato@softa.com.br>
2010-10-15 20:03:49 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railerim::Application.config.secret_token = 'fae0ecd7713579be3ff09dac052e4a59e2e4a4ed0e445547010005f1f95bd10167bd85ca2640b792c96a53d69fd1a973a20b2f1a826eba3fa5380df550301ea9'

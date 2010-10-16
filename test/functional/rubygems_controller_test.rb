058e91b06f49cd6b64a36fa0524f6dbb163df208
Diogo Biazus <diogob@gmail.com>
2010-10-16 19:42:27 -0300

    Fixed rubygem users in show view, now they do not repeat anymore.

--- test/functional/rubygems_controller_test.rb
+++ test/functional/rubygems_controller_test.rb
@@ -17,6 +17,17 @@ class RubygemsControllerTest < ActionController::TestCase
     assert_select '#gem_name', 'rails'
   end
 
+  test "should assign distinct gem users" do
+    u = create_user
+    g = create_rubygem
+    create_dependency(:rubygem => g, :repository => create_repository(:user => u))
+    create_dependency(:rubygem => g, :repository => create_repository(:user => u))
+    get :show, :id => g.to_param
+    assert_response :success
+    assert_equal [u], assigns(:rubygem).users
+    assert_equal 1, assigns(:rubygem).users.size
+  end
+
   {:new => :get, :edit => :get, :create => :post, :update => :put, :destroy => :delete}.each{|k,v|
     test "should not get #{k}" do
       assert_raises AbstractController::ActionNotFound do

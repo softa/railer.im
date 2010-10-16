636f15f19d92f73006033e1acb9f410c87d0e648
Softa <contato@softa.com.br>
2010-10-16 10:50:37 -0400

    another commit without a significant comment. while in rumble do as rumblers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Railerim::Application.routes.draw do
   resources :teams, :only => [:show]
-  resources :users, :only => [:show,:create,:update]
-  resources :rubygems, :only => [:index,:show]
+  resources :users, :only => [:show, :create, :update]
+  resources :rubygems, :only => [:index, :show]
   resources :gems, :controller => :rubygems, :as => :rubygems
   match 'search' => 'home#search', :as => :search
-  resource :user_sessions, :only => [:create,:destroy]
+  resource :user_sessions, :only => [:create, :destroy]
   #TODO remover isso depois?
   match 'home/email' => 'home#email'
   root :to => "home#index"

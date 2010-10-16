cb679b4c9c150f0a5ffaa1c0350fff581218c52d
Leonardo Tartari <ltartari@gmail.com>
2010-10-16 05:56:03 -0300

    [profile] gravatar helper and other stuff. hard to know. very sleepy. must focus.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
 
   def index
   end
-  
-  def email
-    render :text => UserMailer.confirm_email(User.first).body
-  end
-   
+
+  # def email
+  #   render :text => UserMailer.confirm_email(User.first).body
+  # end
+
 end

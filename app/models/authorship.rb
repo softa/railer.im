674b3d5a0190a179c7c235e52b98f16f14f71cd3
Diogo Biazus <diogob@gmail.com>
2010-10-16 17:32:18 -0300

    Now we have a scope for unbound authorships

--- app/models/authorship.rb
+++ app/models/authorship.rb
@@ -1,4 +1,6 @@
 class Authorship < ActiveRecord::Base
   belongs_to :author, :class_name => 'User', :foreign_key => :user_id
   belongs_to :rubygem
+
+  scope :unbound, where("user_id IS NULL")
 end

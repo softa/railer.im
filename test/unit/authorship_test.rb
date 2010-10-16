e424b65e4f36280953875c7ebc1b92373d3e76a4
Diogo Biazus <diogob@gmail.com>
2010-10-16 01:44:46 -0300

    now we can recomend users :)

--- test/unit/authorship_test.rb
+++ test/unit/authorship_test.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class AuthorshipTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
+  test "" do
     assert true
   end
 end

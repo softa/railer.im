581af35741d0e073573418ce28c1b7351187ccc6
Leonardo Tartari <ltartari@gmail.com>
2010-10-16 11:34:43 -0300

    refactor layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
 module ApplicationHelper
 
   def gravatar(gravatar_id, size = 48)
-    image_tag("http://gravatar.com/avatar/" + gravatar_id + "?s=#{size}")
+    image_tag("http://gravatar.com/avatar/" + gravatar_id + "?s=#{size}", :class => 'image')
   end
   
   def nice_link url
-    link_to url.gsub(/http\:\/\/(www.)?/,''), url
+    link_to url.gsub(/http\:\/\/(www.)?/,''), url, :target => :_blank
   end
 
   def thumbalizr url, width = 250 
-    image_tag "http://api.thumbalizr.com/?url=%s&width=%s&api_key=%s&quality=%s" % [url, width, '33b0bc3987ab2e24180562fbbcfde491', '100']
+    image_tag "http://api.thumbalizr.com/?url=%s&width=%s&api_key=%s&quality=%s" % [url, width, '33b0bc3987ab2e24180562fbbcfde491', '100'], :class => 'image'
   end
 
   def tweet_this text
-    link_to image_tag('social/twitter_32.png'), "http://twitter.com/?status=#{CGI.escape(text)}"
+    link_to image_tag('social/twitter_32.png'), "http://twitter.com/?status=#{CGI.escape(text)}", :target => :_blank
   end
 
 end

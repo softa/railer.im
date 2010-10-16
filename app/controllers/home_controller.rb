a99f7f56b7606f2be7bac32bb8ad00674c04cfba
Leonardo Tartari <ltartari@gmail.com>
2010-10-16 03:44:06 -0400

    pull

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@ class HomeController < ApplicationController
 
   def index
   end
-
+  
+  def email
+    Mailer.create_welcome(current_client.users.first).body
+  end
+   
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,11 @@
 class UserMailer < ActionMailer::Base
+
+  layout 'mailers'
+
   default :from => "railer.im <contact@railer.im>"
 
   def confirm_email user
     mail :to => user.email, :subject => 'Welcome to railer.im'
   end
+
 end

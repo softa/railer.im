1c2636013ff47c7c8e30b448623cb6b330d65408
Softa <contato@softa.com.br>
2010-10-16 15:12:24 -0400

    another commit without a significant comment. while in rumble do as rumblers.

---
+++ db/migrate/20101016185623_create_locations_view.rb
@@ -0,0 +1,12 @@
+class CreateLocationsView < ActiveRecord::Migration
+  def self.up
+    execute %(
+    CREATE VIEW locations AS
+    SELECT to_char(lat, '999D999') as lat, to_char(lng, '999D999') as lng, count(*) as total FROM users GROUP BY 1,2 ORDER BY 3 desc
+    )
+  end
+
+  def self.down;
+    execute "DROP VIEW locations;"
+  end
+end

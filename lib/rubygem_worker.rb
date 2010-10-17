ac60c20efd8a9f29fb75ff47e09f17c214d93c0d
Softa <contato@softa.com.br>
2010-10-17 14:54:21 -0400

    another commit without a significant comment. while in rumble do as rumblers.

--- lib/rubygem_worker.rb
+++ lib/rubygem_worker.rb
@@ -12,16 +12,16 @@ class RubygemWorker
     end
     data = JSON.parse(json)
     rubygem.update_attributes :description => data['info'],
-      :downloads => data['downloads'],  
-      :version => data['version'],  
-      :version_downloads => data['version_downloads'],  
-      :homepage_uri => data['homepage_uri'],  
-      :authors_names => data['authors'],  
-      :project_uri => data['project_uri'],  
-      :gem_uri => data['gem_uri'],  
-      :source_code_uri => data['source_code_uri'],  
-      :bug_tracker_uri => data['bug_tracker_uri'],  
-      :wiki_uri => data['wiki_uri'],  
+      :downloads => data['downloads'],
+      :version => data['version'],
+      :version_downloads => data['version_downloads'],
+      :homepage_uri => data['homepage_uri'],
+      :authors_names => data['authors'],
+      :project_uri => data['project_uri'],
+      :gem_uri => data['gem_uri'],
+      :source_code_uri => data['source_code_uri'],
+      :bug_tracker_uri => data['bug_tracker_uri'],
+      :wiki_uri => data['wiki_uri'],
       :mailing_list_uri => data['mailing_list_uri'],
       :documentation_uri => data['documentation_uri']
   end

e632f6bca461d161c60c425e6a8f2a45ee299715
Softa <contato@softa.com.br>
2010-10-17 13:45:20 -0400

    another commit without a significant comment. while in rumble do as rumblers.

--- lib/score_worker.rb
+++ lib/score_worker.rb
@@ -1,7 +1,7 @@
 class ScoreWorker
   def self.perform
     ActiveRecord::Base.connection.execute %(
-    UPDATE users SET score = (SELECT score FROM scores WHERE scores.id = users.id)::int;
+    UPDATE users SET score = coalesce((SELECT score FROM scores WHERE scores.id = users.id)::int,0);
     UPDATE users SET level = log(score+1)::int; 
     )
   end

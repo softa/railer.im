af3f49801770021485883288c7551f90f48c3508
Diogo Biazus <diogob@gmail.com>
2010-10-17 13:19:46 -0200

    changed those crazy tables for lists

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -16,114 +16,34 @@ $.fn.search = function(results) {
 }
 
 // Here we format data to show inside the JQuery target element
+// functio
 $.fn.show_results = function(data) {
-  /*
-              <table class="best_match">
-                <tr>
-                  <th>
-                    Best match
-                  </th>
-                  <td>
-                    <img alt="Star" src="/images/icons/star.png?1287204302">
-                    <strong>lorem ipsum</strong>
-                  </td>
-                </tr>
-              </table>
-              */
-/*
-                <tr>
-                  <th>
-                    User
-                  </th>
-                  <td>
-                    <table>
-                      <tr>
-                        <td>
-                          <img alt="User-silhouette" src="/images/icons/user-silhouette.png?1287204302">
-                        </td>
-                        <td>
-                        </td>
-                      </tr>
-                    </table>
-                  </td>
-                </tr>
-                <tr>
-                  <th>Gems</th>
-                  <td>
-                    <table>
-                      <tr>
-                        <td>
-                          <img alt="Ruby" src="/images/icons/ruby.png?1287204302">
-                        </td>
-                      </tr>
-                    </table>
-                  </td>
-                </tr>
-                <tr>
-                  <th>Teams</th>
-                  <td>
-                    <table>
-                      <tr>
-                        <td>
-                          <img alt="Rumble_small" height="16" src="/images/rumble_small.png?1287242283" width="16">
-                        </td>
-                      </tr>
-                    </table>
-                  </td>
-                </tr>
-                <tr>
-                  <th>Companies</th>
-                  <td>
-                    <table>
-                      <tr>
-                        <td>
-                          <img alt="Briefcase" src="/images/icons/briefcase.png?1287204302">
-                        </td>
-                      </tr>
-                    </table>
-                  </td>
-                </tr>
-                <tr>
-                  <th>Location</th>
-                  <td>
-                    <table>
-                      <tr>
-                        <td>
-                          <img alt="Marker" src="/images/icons/marker.png?1287204302">
-                        </td>
-                      </tr>
-                    </table>
-                  </td>
-                </tr>
-              </table>
-              '
-              */
   var tophit = {"rank" : -1};
   var html_tables = {"users": "", "rubygems" : "", "teams" : "", "companies": "", "locations" : "", "best" : "" };
   $.each(data["users"], function(key, row){
       tophit = (row["rank"] > tophit["rank"] ? row : tophit);
-      html_tables["users"] += '<tr><td><img src="http://gravatar.com/avatar/' + row["gravatar_id"] + '?s=16" /></td><td>' + row["label"] + '</td></tr>';
+      html_tables["users"] += '<li><img src="http://gravatar.com/avatar/' + row["gravatar_id"] + '?s=16" />' + row["label"] + '</li>';
     })
   $.each(data["rubygems"], function(key, row){
       tophit = (row["rubygems"] > tophit["rank"] ? row : tophit);
-      html_tables["rubygems"] += '<tr><td><img src="/images/icons/ruby.png" /></td><td>' + row["label"] + '</td></tr>';
+      html_tables["rubygems"] += '<li><img src="/images/icons/ruby.png" />' + row["label"] + '</li>';
     })
   $.each(data["teams"], function(key, row){
       tophit = (row["rank"] > tophit["rank"] ? row : tophit);
-      html_tables["teams"] += '<tr><td></td><td>' + row["label"] + '</td></tr>';
+      html_tables["teams"] += '<li>' + row["label"] + '</li>';
     })
   $.each(data["companies"], function(key, row){
       tophit = (row["rank"] > tophit["rank"] ? row : tophit);
-      html_tables["companies"] += '<tr><td></td><td>' + row["label"] + '</td></tr>';
+      html_tables["companies"] += '<li>' + row["label"] + '</li>';
     })
   $.each(data["locations"], function(key, row){
       tophit = (row["rank"] > tophit["rank"] ? row : tophit);
-      html_tables["locations"] += '<tr><td></td><td>' + row["label"] + '</td></tr>';
+      html_tables["locations"] += '<li>' + row["label"] + '</li>';
     })
 
-  html_tables["best"] = (tophit["rank"] < 0 ? "" : '<tr><td></td><td><img src="/images/icons/star.png"><strong> ' + tophit["label"] + '</strong></tr>');
+  html_tables["best"] = (tophit["rank"] < 0 ? "" : '<li><img src="/images/icons/star.png"><strong> ' + tophit["label"] + '</strong></li>');
   $.each(html_tables, function(k,v){
-      $('#' + k + '_search_list').html((v == '' ? '<tr><td>None found</td></tr>' : v));
+      $('#' + k + '_search_list').html((v == '' ? '<li>None found</li>' : v));
     })
 }
 

e9d3bc0277cf5991142328ca56660ef89ec8581f
Diogo Biazus <diogob@gmail.com>
2010-10-17 08:11:51 -0200

    now level is not null

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,17 @@
 class Search < ActiveRecord::Base
+  @@threshold = nil
+  def self.similarity_threshold
+    @@threshold
+  end
+
+  def self.set_similarity_threshold threshold
+    return nil unless threshold.instance_of?(Fixnum) or threshold.instance_of?(Float)
+    @@threshold = threshold
+    User.set_similarity_threshold(@@threshold)
+  end
+
+  def self.results_for(query)
+    sql = User.rank_by_similarity(query).to_sql
+    connection.execute(sql)
+  end
 end
--- test/unit/search_test.rb
+++ test/unit/search_test.rb
@@ -1,10 +1,13 @@
 require 'test_helper'
 
 class SearchTest < ActiveSupport::TestCase
-  test "best match should return entry_type and key" do
-    create_user.update_attributes :name => 'Pedro Axelrud', :login => 'pedroaxl', :email => 'pedroaxl@gmail.com'
+  test "query should return entry_type, key, label, gravatar_id" do
+    create_user.update_attributes :name => 'Pedro Axelrud', :login => 'pedroaxl', :email => 'pedroaxl@gmail.com', :gravatar_id => '123'
     create_user.update_attributes :name => 'João Pedro', :login => 'jpedro', :email => 'jpedro@gmail.com'
     create_user.update_attributes :name => 'Seu Pedro', :login => 'spedro', :email => 'spedro@gmail.com'
-    User.set_similarity_threshold(0.5)
+    Search.set_similarity_threshold(0.5)
+    result = Search.results_for('pedroaxl@gmail.com')
+    assert_equal({'entry_type' => 'user', 'key' => 'pedroaxl', 'label' => 'Pedro Axelrud', 'gravatar_id' => '123', 'rank' => '1'}, result[0])
+    assert_equal 3, result.num_tuples
   end
 end

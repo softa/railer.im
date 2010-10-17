6fe601063f90486f23d733fbc3d59797309aa63c
Pedro Axelrud <pedroaxl@gmail.com>
2010-10-17 19:23:51 -0400

    not using mailee gem to send emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,10 +3,14 @@ class UserMailer < ActionMailer::Base
   default :from => "railer.im <contact@railer.im>"
 
   def confirm_email user
+    user.reset_perishable_token!
+    user.reload
     @user = user
     mail :to => user.email, :subject => '[railer.im] Welcome railer'
   end
   def reset_email user
+    user.reset_perishable_token!
+    user.reload
     @user = user
     mail :to => user.email, :subject => '[railer.im] Password reset'
   end

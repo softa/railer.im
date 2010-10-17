14dfc0294ef3f133357d33ab7317a26aec1295e6
Softa <contato@softa.com.br>
2010-10-17 08:29:36 -0400

    another commit without a significant comment. while in rumble do as rumblers.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,6 +3,6 @@ class TeamsController < InheritedResources::Base
 protected
   before_filter :set_near_teams
   def set_near_teams
-    @near_teams = Team.limit(5).order('random()')
+    @near_teams = Team.limit(12).order('random()').where("thumbnail <> ''")
   end
 end
